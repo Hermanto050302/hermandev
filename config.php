38b56920d24c577195295e5f5c421ae7eec6bd75
Hermanto050403 <hermantoimade45@gmail.com>
2020-10-14 22:32:54 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 
-$databaseHost = 'localhost';
-$databaseName = 'db_portofolio';
-$databaseUsername = 'root';
-$databasePassword = 'root';
+$databaseHost = 'sql300.epizy.com';
+$databaseName = 'epiz_26614398_w866';
+$databaseUsername = 'epiz_26614398';
+$databasePassword = 'u3QOpeoFDieiTs';
 
 $mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
 
